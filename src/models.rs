06bae53dcb807c4a044315473641d92291a081fc
El RIDO <elrido@gmx.net>
2020-03-26 22:02:10 +0100

    validate and return version

--- src/models.rs
+++ src/models.rs
@@ -6,19 +6,20 @@ use regex::Regex;
 use serde::Serialize;
 
 const TITLE: &str = "Instance Directory";
-
-//type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+const LATEST_PRIVATEBIN_VERSION: &str = "1.3.4";
 
 pub struct Instance {
     pub url: String,
+    pub version: String,
     pub country_id: [u8; 2],
 }
 
 impl Instance {
-    pub fn new(url: String, country_code: &str) -> Instance {
+    pub fn new(url: String, version: String, country_code: &str) -> Instance {
         let mut country_chars = country_code.chars();
         Instance {
             url: url,
+            version: version,
             country_id: [
                 country_chars.next().unwrap() as u8,
                 country_chars.next().unwrap() as u8
@@ -34,37 +35,42 @@ pub struct PrivateBin {
 
 impl PrivateBin {
     pub async fn new(url: String) -> Result<PrivateBin, String> {
-        let (is_valid, country_code, validation_status) = Self::validate(&url).await.unwrap();
-        if is_valid {
+        let validation = Self::validate(&url).await;
+        if validation.is_ok() {
+            let (country_code, version) = validation.unwrap();
             return Ok(
                 PrivateBin {
-                    instance: Instance::new(url, country_code),
+                    instance: Instance::new(url, version, country_code),
                 }
             )
         }
-        Err(validation_status)
+        Err(validation.unwrap().0.to_string())
     }
 
-    async fn validate(url: &String) -> Result<(bool, &'static str, String), Box<dyn std::error::Error + Send + Sync>> {
+    async fn validate(url: &String) -> Result<(&'static str, String), Box<dyn std::error::Error + Send + Sync>> {
         let https = HttpsConnector::new();
         let client = Client::builder().build::<_, Body>(https);
         let uri = url.parse()?;
         let mut res = client.get(uri).await?;
         if res.status() != 200 {
-            return Ok((false, "UN", format!("Web server responded with status code {}.", res.status())))
+            return Err(format!("Web server responded with status code {}.", res.status()).into())
         }
 
-        let version_re = Regex::new(r"js/privatebin.js\?(\d+\.\d+\.*\d*)").unwrap();
+        let privatebin_version_re = Regex::new(r"js/privatebin.js\?(\d+\.\d+\.*\d*)").unwrap();
+        let zerobin_version_re = Regex::new(r"js/zerobin.js\?Alpha%20(\d+\.\d+\.*\d*)").unwrap();
         while let Some(chunk) = res.body_mut().data().await {
             let a_chunk = &chunk?;
             let chunk_str = std::str::from_utf8(&a_chunk).unwrap();
-            let matches = version_re.captures(chunk_str);
-            if matches.is_some() {
-                println!("Version: {}", &matches.unwrap()[1]);
-                return Ok((true, "AQ", String::from("")))
+            let privatebin_matches = privatebin_version_re.captures(chunk_str);
+            if privatebin_matches.is_some() {
+                return Ok(("AQ", privatebin_matches.unwrap()[1].to_string()))
+            }
+            let zerobin_matches = zerobin_version_re.captures(chunk_str);
+            if zerobin_matches.is_some() {
+                return Ok(("AQ", zerobin_matches.unwrap()[1].to_string()))
             }
         }
-        return Ok((false, "UN", format!("The URL {} doesn't seem to be a PrivateBin instance.", url)))
+        Err(format!("The URL {} doesn't seem to be a PrivateBin instance.", url).into())
     }
 }
 
@@ -73,6 +79,25 @@ async fn test_privatebin() {
     let url = String::from("https://privatebin.net");
     let privatebin = PrivateBin::new(url.clone()).await.unwrap();
     assert_eq!(privatebin.instance.url, url);
+    assert_eq!(privatebin.instance.version, LATEST_PRIVATEBIN_VERSION);
+    assert_eq!(privatebin.instance.country_id, ['A' as u8, 'Q' as u8]);
+}
+
+#[tokio::test]
+async fn test_zerobin() {
+    let url = String::from("https://sebsauvage.net/paste/");
+    let privatebin = PrivateBin::new(url.clone()).await.unwrap();
+    assert_eq!(privatebin.instance.url, url);
+    assert_eq!(privatebin.instance.version, "0.19.2");
+    assert_eq!(privatebin.instance.country_id, ['A' as u8, 'Q' as u8]);
+}
+
+#[tokio::test]
+async fn test_privatebin_http() {
+    let url = String::from("http://zerobin-test.dssr.ch/");
+    let privatebin = PrivateBin::new(url.clone()).await.unwrap();
+    assert_eq!(privatebin.instance.url, url);
+    assert_eq!(privatebin.instance.version, LATEST_PRIVATEBIN_VERSION);
     assert_eq!(privatebin.instance.country_id, ['A' as u8, 'Q' as u8]);
 }
 
