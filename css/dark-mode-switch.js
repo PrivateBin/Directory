d7a6a787de50577df4fbad8361332fd1888cffa9
El RIDO <elrido@gmx.net>
2020-03-30 22:26:43 +0200

    dark mode

---
+++ css/dark-mode-switch.js
@@ -0,0 +1,46 @@
+(function() {
+  var darkSwitch = document.getElementById("darkSwitch");
+  if (darkSwitch) {
+    initTheme();
+    darkSwitch.addEventListener("change", function(event) {
+      resetTheme();
+    });
+    function enableDark() {
+      document.body.setAttribute("data-theme", "dark");
+      document.querySelectorAll("table").forEach(
+        function(table) {
+          table.classList.add("table-dark");
+        }
+      );
+    }
+    function disableDark() {
+      document.body.removeAttribute("data-theme");
+      document.querySelectorAll("table").forEach(
+        function(table) {
+          table.classList.remove("table-dark");
+        }
+      );
+    }
+    function initTheme() {
+      var darkThemeSelected =
+        localStorage.getItem("darkSwitch") !== null &&
+        localStorage.getItem("darkSwitch") === "dark";
+      darkSwitch.checked = darkThemeSelected;
+      darkThemeSelected
+        ? enableDark()
+        : disableDark();
+      if (darkThemeSelected) {
+        window.addEventListener('DOMContentLoaded', enableDark, false);
+      }
+    }
+    function resetTheme() {
+      if (darkSwitch.checked) {
+        enableDark();
+        localStorage.setItem("darkSwitch", "dark");
+      } else {
+        disableDark();
+        localStorage.removeItem("darkSwitch");
+      }
+    }
+  }
+})();
