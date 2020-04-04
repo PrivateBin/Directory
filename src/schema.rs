e6439cb0b511a186f0076afb27624c08551f6610
El RIDO <elrido@gmx.net>
2020-04-04 09:57:41 +0200

    add a checks table #8

--- src/schema.rs
+++ src/schema.rs
@@ -1,4 +1,13 @@
 table! {
+    checks (id) {
+        id -> Integer,
+        updated -> Timestamp,
+        up -> Bool,
+        instance_id -> Integer,
+    }
+}
+
+table! {
     instances (id) {
         id -> Integer,
         url -> Text,
@@ -9,3 +18,10 @@ table! {
         attachments -> Bool,
     }
 }
+
+joinable!(checks -> instances (instance_id));
+
+allow_tables_to_appear_in_same_query!(
+    checks,
+    instances,
+);
